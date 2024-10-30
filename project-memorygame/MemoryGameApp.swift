9482ff1c0253e129d50a2dacfe862d37bc07b2e4
aarohasapkota <63642081+aarohasapkota@users.noreply.github.com>
2024-10-29 23:44:17 -0400

    Adding FIles

---
+++ project-memorygame/MemoryGameApp.swift
@@ -0,0 +1,18 @@
+//
+//  FlashcardApp.swift
+//  Flashcard
+//
+//  Created by Aaroha Sapkota on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+            ContentView()
+        }
+    }
+}
